7c7f80b0a689f9d36174a16d1d675249a4eb49e9
5t4r-h4rf4 <bp0250@gmail.com>
2022-04-20 09:40:11 +0700

    Bintang/Users dan Roles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,16 @@
                                 </li>
                             @endif
                         @else
+                            @can('user-list')
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('users.index') }}">User</a>
+                                </li>
+                            @endcan
+                            @can('role-list')
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('roles.index') }}">Role</a>
+                                </li>
+                            @endcan
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -68,7 +78,7 @@
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                                                                                                                                                                             document.getElementById('logout-form').submit();">
+                                                                                                                                                                                                                                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
