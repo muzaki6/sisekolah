06f42bcbb541dcdb50cae9a6c7762c424b47322d
unknown <zakizuhdi360@gmail.com>
2022-05-24 09:01:48 +0700

    edit-zaki

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -11,8 +11,9 @@ class Siswa extends Model
     use HasFactory;
     protected $table = 'siswas';
     protected $guarded = ['id'];
+    protected $primaryKey = 'id';
 
     protected $fillable = [
-        'id', 'content'
+        'id', 'no_induk', 'nama', 'jenis_kelamin', 'agama', 'alamat', 'tahun_masuk', 'created_at', 'updated_at'
     ];
 }
