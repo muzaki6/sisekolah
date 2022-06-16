c258c4069a3e7b786541bb42c09c124df3b802d2
berlyann15 <berlyan31@gmail.com>
2022-06-16 14:38:59 +0700

    komentar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Si Sekolah</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -404,6 +404,8 @@
     <!-- Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 
     <style>
         body {
@@ -413,65 +415,148 @@
     </style>
 </head>
 
+
 <body class="antialiased" style="background-image: url('storage/image-background/background1.jpg')">
-    <nav class="container-fluid navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="bg-dark col-md-10">
-            <a class="navbar-brand" href="#">Sistem Informasi Sekolah</a>
+  <nav class="container-fluid navbar navbar-expand-lg navbar-light bg-light" >
+            <!-- <a class="navbar-brand" href="#">Sistem Informasi Sekolah</a> -->
+            <img class="navbar-brand" src="{{asset('image/image-icon/logo2.png')}}"style="width: 140px;" alt="">
         </div>
         <div class="collapse navbar-collapse col-md-2 justify-content-end" id="navbarScroll">
             <ul class="navbar-nav" style="--bs-scroll-height: 100px;">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{ route('login') }}">Login</a>
+            <ul class="nav navbar-nav">
+            <li class="nav-item mx-2">
+                    <a class="nav-link " href="#">Beranda</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Register</a>
+                <li class="nav-item mx-2">
+                    <a class="nav-link" href="#">Profil</a>
+                </li>  
+                <li class="nav-item dropdown mx-2">
+                    <!-- <a class="nav-link" href="#"></a> -->
+                 
+            <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                  Program Keahlian
+            </a>  
+        
+             <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                 <li><a class="dropdown-item" href="#">Teknik Komputer dan Jaringan</a></li>
+                 <li><a class="dropdown-item" href="#">Administrasi Perkantoran</a></li>
+                <li><a class="dropdown-item" href="#">Rekayasa Perangkat Lunak</a></li>
+            </ul>
+           
+                </li>    
+                <li class="nav-item mx-2">
+                    <a class="nav-link" href="#">Galeri</a>
+                </li>    
+                <li class="nav-item mx-3">
+                    <div class="btn-group">
+                    <a class="nav-link btn " style="color: #0a2351;border: 1px solid #0a2351;"; aria-current="page" href="{{ route('login') }}">Login</a>
+                    <a class="nav-link btn text-white" style="background:#0a2351; href="#">Register</a>
+                    </div>
                 </li>
             </ul>
-        </div>
+        </ul>
     </nav>
-    <div class="container mt-5 mb-5 col-md-12 bg-success">
+    <!-- <div class="container mt-5 mb-5 col-md-12 bg-success">
+    </div> -->
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{asset('image/image-icon/siswa.jpg')}}" class="d-block w-100 h-50" alt="...">
     </div>
-    <div class="container">
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    <div class="container mt-3">
         <div class="row">
-            <div class="col-md-8">
-                <h2><b>SELAMAT DATANG</b></h2>
+            <div class="col-md-6">
+                <h3><b>Prakata Kepala Sekolah</b></h3>
+                <p>Assalamu'alaikum wr.wb.</p>
+                <p class="left">Puji syukur kepada Allah SWT, Tuhan Yang Maha Esa yang telah memberikan rahmat dan anugerahNya sehingga website SiSekolah ini dapat terbit. Salah satu tujuan dari website ini adalah untuk menjawab akan setiap kebutuhan informasi dengan memanfaatkan sarana teknologi informasi yang ada. Kami sadar sepenuhnya dalam rangka memajukan pendidikan di era berkembangnya Teknologi Informasi yang begitu pesat, sangat diperlukan berbagai sarana prasarana yang kondusif, kebutuhan berbagai informasi siswa, guru, orangtua maupun masyarakat. Semoga dengan adanya website ini dapat membantu dan bermanfaat, terutama informasi yang berhubungan dengan pendidikan, ilmu pengetahuan dan informasi. Kami mengharapkan masukan dari berbagai pihak untuk website ini agar kami terus belajar dan meng-update diri, sehingga tampilan, isi dan mutu website akan terus berkembang dan lebih baik nantinya.</p>
+                <p>Terima kasih 
+                    Wassalamu'alaikum wr.wb.
+                </p>
             </div>
-            <div class="col-md-4">
-                <h3></h3>
+            <div class="col-md-3">
+                <h3><b>Program Sekolah</b></h3>
+                <p class="left">
+                Adanya berbagai kegiatan pelatihan dari luar dan praktek yang sering dilakukan di sekolah ini memudahkan siswa berhubungan dengan dunia usaha dan dunia industri juga dalam menghadapi kemajuan teknologi yang saat ini berkembang dengan pesat dan siap terjun dalam dunia kerja.
+                </p>
+            </div>
+            <div class="col-md-3">
+                <h3><b>Program Keahlian</b></h3>
+                <p class="left">
+                SMK ini memiliki 8 program keahlian ,berikut daftar lengkapnya ada Administrasi Perkantoran, Perbankan, Akuntansi, Pemasaran, Teknik Komputer Jaringan, Multimedia, Pemasaran, Usaha Perjalanan Wisata.
+                </p>
             </div>
         </div>
     </div>
     <div class="container mt-3">
         <div class="row">
-            <div class="col-md-8">
-                <h6><b>PROFIL SEKOLAH</b></h6>
+            <div class="col-md-6">
+                <h3><b>Profil Sekolah</b></h3>
+                <p class="left">
+                Berdirinya SMEA Negeri Kediri adalah prakarsa dari Wali Kota Kediri yaitu Bapak Anwar Zainudin pada bulan Agustus 1965 dengan menempati gedung SMP Negeri 5  Kediri di Kecamatan Pesantren. Namun seiring dengan perkembangannya pada bulan Agustus 1966 pindah ke SMP Negeri 3 Kediri di Jalan Joyoboyo Kediri. Pada tahun 1968 SKOPMA (Sekolah Koperasi Tingkat menengah Atas) dengan pimpinan bapak Raboen Koesharto, BSc. berintegrasi ke SMEA Negeri Kediri dan menempati gedung baru di Jl. Monginsidi 36. Selanjutnya dengan semakin banyaknya minat dari Masyarakat Kediri dan sekitarnya terhadap SMEA Negeri Kediri maka pada tahun 1986 menempati gedung baru  di jalan Veteran no. 5 yang dibangun atas bantuan dana dari ADB (Asian Development Bank) hingga sekarang, namun untuk gedung yang berada di jalan Monginsidi masih tetap dipakai untuk kegiatan pembelajaran kelas X (sepuluh) kompetensi keahlian Bisnis Manajemen dan Keuangan
+                </p>
             </div>
-            <div class="col-md-4">
-                <h3></h3>
-            </div>
-            <div class="col-md-12" style="text-align: justify;">
-                <font size="2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                    has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                    galley of type and scrambled it to make a type specimen book. It has survived not only five
-                    centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
-                    popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
-                    more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
-                    Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                    has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                    galley of type and scrambled it to make a type specimen book. It has survived not only five
-                    centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
-                    popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
-                    more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
-                    Ipsum.</font>
-            </div>
-            <div class="col-md-12 mt-3">
-                <h6><b>0353-0000000</b></h6>
-            </div>
-            <div class="col-md-12 mt-3">
-                <h6><b>JALAN MERDEKA UTARA NO. 01 KOTA BARU</b></h6>
+            <div class="col-md-6">
+                <h3><b>Info Kegiatan</b></h3>
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                gambar  
+                            </div>
+                            <div class="col-md-8">
+                                <h5>Apel Pagi dalam rangka memperingatu HUT Pramuka asek</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, non! Laborum,</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                gambar  
+                            </div>
+                            <div class="col-md-8">
+                                <h5>Upacara hari Senin</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, non! Laborum,</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                gambar  
+                            </div>
+                            <div class="col-md-8">
+                                <h5>Siswa Peraih Medali Emas</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, non! Laborum,</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
+    <div class="p-5 bg-secondary text-white" style="height:50px;"></div>
 </body>
 
 </html>
